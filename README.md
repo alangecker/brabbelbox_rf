cc87371605ebd0b23b91ae70bf67d56206a8501d
Matthias Larisch <mail@matthias-larisch.de>
2020-07-26 16:49:07 +0200

    First POC with some description

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# brabbelbox_audio
+# brabbelbox_rf
+
+Implements the SDR part of brabbelbox project: Sticking gnuradio blocks together to do a simultaneous multi stream FM transmission.
+
+## System requirements
+
+* Gnuradio 3.8 (but should also run on 3.7)
+* osmocom compatible SDR, the code might be tuned for hackrf but should easily be portable
+
+## What does it do
+* Take N audio streams from a sound card (N being around 3..8)
+* Modulate them on adjustable frequencies in a 6..8 MHz band (even more if you throw enough computing power at it)
+
+## What still needs to be done
+* API to set frequencies
+* Care about low latency: Provide HackRF patches to lower buffer size
+* Use a gnuradio source that provides adaptive resampling (e.g. add zita-resampler to the gr-audio jack source)
+
+## How to use it?
+When you open the gnuradio file `polyphase_single.grc` you have a not too complex flow graph:
+* variable `aud_rate`: Sets audio sampling rate (suggest to use 48 kHz everywhere)
+* variable `mod_rate`: Sets the sampling rate used for modulation (use at least 200 kHz for wide band FM), will also define the channel spacing
+* variable `num_banks`: Sets the number of available channels. `mod_rate` x `num_banks` sets the RF bandwidth that will be sent by your SDR. (Suggest to use 4..20 MHz with HackRF)
+* variable `taps`: fine tune low pass filter used in polyphase synthesizer. If you want to change something, you already know why and what :-)
+* variable `taps_rs`: fine tune low pass filter used in rational resampler (audio -> modulation resampler). Shouldn't need touching as well.
+* Audio source on the left: Multi channel audio source
+* resampler, WBFM transmit blocks: These out of the box gnuradio blocks perform really good and shouldn't need any modifications.
+* polyphase synthesizer: This block performs the magic of combining all the fm signals together.
+  * taps: The filter here is responsible to provide a clear channel separation without destroying the usable signal. 
+  * Channel map: Define the relationship of the connected channels to the transmission frequencies. Channel 0 is transmitted on 0 Hz (e.g. center frequency), channel 1 on 0+`mod_rate` (e.g. 200 kHz), channel `num_banks`/2 is on both ends of the spectrum (so shouldn't be used) and `num_banks`/2+1 starts at the lower end, whereby `num_banks`-1 is on 0-`mod_rate`, e.g. -200 kHz.
+* osmocom sink: Set your SDR parameters here.
+  * hackrf likes to transmit a signal at least 2 MHz wide, better a bit wider
+  * use the device argument `buffers=1` for a decreased latency
+  * set the frequency to your center frequency
+  * tune the transmission power with RF gain (0 or 14 dB TX amplifier) and IF Gain (0..42 dB in 1 dB increments)
+
+Never ever connect your SDR to an antenna as this might be illegal in your country! Always use a dummy load or a direct connection to another SDR for receiving (make sure to use a low transmission power to not brick your second SDR).
+
---
+++ grc_examples/polyphase.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: multi_fm_transmit
+# Author: matthias
+# GNU Radio version: 3.8.1.0
+
+from gnuradio import analog
+from gnuradio import audio
+from gnuradio import filter
+from gnuradio.filter import firdes
+from gnuradio import gr
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+import osmosdr
+import time
+
+
+class polyphase(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "multi_fm_transmit")
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.num_banks = num_banks = 30
+        self.mod_rate = mod_rate = 200000
+        self.taps_rs = taps_rs = firdes.low_pass_2(25.0, 25.0, 0.1, 0.1, 60, firdes.WIN_KAISER, 7.0)
+        self.taps = taps = firdes.low_pass_2(8, num_banks*mod_rate, 120e3, 50e3, 80, firdes.WIN_BLACKMAN_HARRIS)
+        self.aud_rate = aud_rate = 48000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.rational_resampler_xxx_0_0_0 = filter.rational_resampler_fff(
+                interpolation=25,
+                decimation=6,
+                taps=taps_rs,
+                fractional_bw=0.3)
+        self.rational_resampler_xxx_0_0 = filter.rational_resampler_fff(
+                interpolation=25,
+                decimation=6,
+                taps=taps_rs,
+                fractional_bw=0.3)
+        self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
+                interpolation=25,
+                decimation=6,
+                taps=taps_rs,
+                fractional_bw=0.3)
+        self.pfb_synthesizer_ccf_0 = filter.pfb_synthesizer_ccf(
+            num_banks,
+            taps,
+            False)
+        self.pfb_synthesizer_ccf_0.set_channel_map([1,9,8])
+        self.pfb_synthesizer_ccf_0.declare_sample_delay(0)
+        self.osmosdr_sink_1 = osmosdr.sink(
+            args="numchan=" + str(1) + " " + "buffers=1"
+        )
+        self.osmosdr_sink_1.set_sample_rate(mod_rate*num_banks)
+        self.osmosdr_sink_1.set_center_freq(96000000, 0)
+        self.osmosdr_sink_1.set_freq_corr(0, 0)
+        self.osmosdr_sink_1.set_gain(0, 0)
+        self.osmosdr_sink_1.set_if_gain(0, 0)
+        self.osmosdr_sink_1.set_bb_gain(0, 0)
+        self.osmosdr_sink_1.set_antenna('', 0)
+        self.osmosdr_sink_1.set_bandwidth(0, 0)
+        self.audio_source_0 = audio.source(aud_rate, 'plughw:UMC1820', False)
+        self.analog_wfm_tx_0_0_0 = analog.wfm_tx(
+        	audio_rate=mod_rate,
+        	quad_rate=mod_rate,
+        	tau=75e-6,
+        	max_dev=75e3,
+        	fh=10e3,
+        )
+        self.analog_wfm_tx_0_0 = analog.wfm_tx(
+        	audio_rate=mod_rate,
+        	quad_rate=mod_rate,
+        	tau=75e-6,
+        	max_dev=75e3,
+        	fh=10e3,
+        )
+        self.analog_wfm_tx_0 = analog.wfm_tx(
+        	audio_rate=mod_rate,
+        	quad_rate=mod_rate,
+        	tau=75e-6,
+        	max_dev=75e3,
+        	fh=10e3,
+        )
+
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_wfm_tx_0, 0), (self.pfb_synthesizer_ccf_0, 0))
+        self.connect((self.analog_wfm_tx_0_0, 0), (self.pfb_synthesizer_ccf_0, 1))
+        self.connect((self.analog_wfm_tx_0_0_0, 0), (self.pfb_synthesizer_ccf_0, 2))
+        self.connect((self.audio_source_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.audio_source_0, 1), (self.rational_resampler_xxx_0_0, 0))
+        self.connect((self.audio_source_0, 2), (self.rational_resampler_xxx_0_0_0, 0))
+        self.connect((self.pfb_synthesizer_ccf_0, 0), (self.osmosdr_sink_1, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.analog_wfm_tx_0, 0))
+        self.connect((self.rational_resampler_xxx_0_0, 0), (self.analog_wfm_tx_0_0, 0))
+        self.connect((self.rational_resampler_xxx_0_0_0, 0), (self.analog_wfm_tx_0_0_0, 0))
+
+
+    def get_num_banks(self):
+        return self.num_banks
+
+    def set_num_banks(self, num_banks):
+        self.num_banks = num_banks
+        self.set_taps(firdes.low_pass_2(8, self.num_banks*self.mod_rate, 120e3, 50e3, 80, firdes.WIN_BLACKMAN_HARRIS))
+        self.osmosdr_sink_1.set_sample_rate(self.mod_rate*self.num_banks)
+
+    def get_mod_rate(self):
+        return self.mod_rate
+
+    def set_mod_rate(self, mod_rate):
+        self.mod_rate = mod_rate
+        self.set_taps(firdes.low_pass_2(8, self.num_banks*self.mod_rate, 120e3, 50e3, 80, firdes.WIN_BLACKMAN_HARRIS))
+        self.osmosdr_sink_1.set_sample_rate(self.mod_rate*self.num_banks)
+
+    def get_taps_rs(self):
+        return self.taps_rs
+
+    def set_taps_rs(self, taps_rs):
+        self.taps_rs = taps_rs
+        self.rational_resampler_xxx_0.set_taps(self.taps_rs)
+        self.rational_resampler_xxx_0_0.set_taps(self.taps_rs)
+        self.rational_resampler_xxx_0_0_0.set_taps(self.taps_rs)
+
+    def get_taps(self):
+        return self.taps
+
+    def set_taps(self, taps):
+        self.taps = taps
+        self.pfb_synthesizer_ccf_0.set_taps(self.taps)
+
+    def get_aud_rate(self):
+        return self.aud_rate
+
+    def set_aud_rate(self, aud_rate):
+        self.aud_rate = aud_rate
+
+
+
+
+
+def main(top_block_cls=polyphase, options=None):
+    if gr.enable_realtime_scheduling() != gr.RT_OK:
+        print("Error: failed to enable real-time scheduling.")
+    tb = top_block_cls()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    tb.start(200000)
+
+    try:
+        input('Press Enter to quit: ')
+    except EOFError:
+        pass
+    tb.stop()
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
